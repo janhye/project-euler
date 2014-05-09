eba83e6a1d8bd6ddd0aa945572203c0bfdb0509b
Ye Jian Hui <jan.h.ye@gmail.com>
2014-05-09 23:17:21 +0800

    updated bignum

--- bignum.lua
+++ bignum.lua
@@ -1,10 +1,11 @@
 local string = string
 local math = math
 local table = table
+--local bit32 = bit32
 
-local Bignum = {}
+local BignumDec = {}
 
-function Bignum.new (digit)
+function BignumDec.new (digit)
   digit = digit or 0
   if type(digit) == "string" then 
     local _, e = string.find(digit, "%d+")
@@ -21,8 +22,8 @@ function Bignum.new (digit)
     data = {}, 
     bits = 9 
   }
-  Bignum.__index = Bignum
-  setmetatable(o, Bignum)
+  BignumDec.__index = BignumDec
+  setmetatable(o, BignumDec)
 
   local c = 1
   repeat
@@ -35,39 +36,50 @@ function Bignum.new (digit)
   return o
 end
 
-function Bignum:add (n)
+function BignumDec:add (n, modify_self)
   n = n or 0
-  local b = Bignum.new()
+
+  local b
+  if modify_self == true then
+    b = self
+  else
+    b = BignumDec.new()
+  end
 
   if type(n) == "number" or type(n) == "string" then
-    n = Bignum.new(n)
+    n = BignumDec.new(n)
   end
 
   local l = math.max(#self.data, #n.data)
-  local extra
+  local extra = 0
   local num
   for i = 1, l do
-    b.data[i] = b.data[i] or 0
     if i > #n.data then
-      num = self.data[i] + b.data[i]
+      num = self.data[i] + extra
     elseif i > #self.data then
-      num = n.data[i] + b.data[i]
+      num = n.data[i] + extra
     else
-      num = self.data[i] + n.data[i] + b.data[i]
+      num = self.data[i] + n.data[i] + extra
     end
 
-    extra = num - 10 ^ b.bits
-    if extra >= 0 then
-      b.data[i] = extra
-      b.data[i + 1] = 1
+    local minus = num - 10 ^ b.bits
+    if minus >= 0 then
+      b.data[i] = minus
+      extra = 1
     else
       b.data[i] = num
+      extra = 0
     end
   end
+
+  if extra > 0 then
+    b.data[l + 1] = extra
+  end
+
   return b
 end
 
-function Bignum:tostring ()
+function BignumDec:tostring ()
   local buf = {}
   for i = #self.data, 1, -1 do 
     local s = tostring(self.data[i])
@@ -82,4 +94,162 @@ function Bignum:tostring ()
   return table.concat(buf)
 end
 
-return Bignum
+function BignumDec:div2 (modify_self)
+  local b, mod
+  if modify_self == true then
+    b = self
+  else
+    b = BignumDec.new()
+  end
+
+  mod = self.data[1] % 2
+
+  for i = 1, #self.data do
+    if i > 1 then
+      if self.data[i] % 2 == 1 then
+        b.data[i - 1] = b.data[i - 1] + 5 * (10 ^ (b.bits - 1))
+      end
+    end
+
+    if i == #self.data and self.data[i] == 1 then
+      if i == 1 then
+        b.data[i] = 0
+      else
+        b.data[i] = nil
+      end
+    else
+      b.data[i] = math.floor(self.data[i] / 2)
+    end
+  end
+
+  return mod, b
+end
+
+function BignumDec:is_zero ()
+  if #self.data == 1 and self.data[1] == 0 then
+    return true
+  else
+    return false
+  end
+end
+
+-- 2 ^ n
+function BignumDec.pow2 (n)
+  local b
+  if n == 0 then
+    b = BignumDec.new(1)
+  elseif n >= 1 then
+    b = BignumDec.new(2)
+  end
+
+  for i = 2, n do
+    b = b:add(b, true)
+  end
+  return b
+end
+
+--[[-------------------------------------------------------------------------
+
+local BignumBin = {}
+
+function BignumBin.new (digit)
+  digit = digit or 0
+  if type(digit) == "string" then 
+    local _, e = string.find(digit, "%d+")
+    if e ~= #digit then 
+      digit = 0 
+    end
+  elseif type(digit) == "number" then
+    digit = tostring(digit)
+  else
+    error("digit type error")
+  end
+
+  local o = {
+    data = {}, 
+    bits = 31
+  }
+  BignumBin.__index = BignumBin
+  setmetatable(o, BignumBin)
+
+  local dec = BignumDec.new(digit)
+  local mod
+  local d = 0
+  local i = 1
+  repeat
+    mod = dec:div2(true)
+    if mod == 1 then
+      d = d + 2 ^ (i - 1)
+    end
+    if i % o.bits == 0 then
+      o.data[#o.data + 1] = d
+      d = 0
+      i = 1
+    else
+      i = i + 1
+    end
+  until dec:is_zero()
+  if d ~= 0 then
+    o.data[#o.data + 1] = d
+  end
+
+  return o
+end
+
+function BignumBin:tostring ()
+  local sum = BignumDec.new(0)
+  for k, v in ipairs(self.data) do
+    for i = 1, self.bits do
+      if bit32.band(self.data[k], 2 ^ (i - 1)) ~= 0 then
+        sum:add(BignumDec.pow2((k - 1) * self.bits + i - 1), true)
+      end
+    end
+  end
+  return sum:tostring()
+end
+
+function BignumBin:add (n, modify_self)
+  n = n or 0
+
+  local b
+  if modify_self == true then
+    b = self
+  else
+    b = BignumBin.new()
+  end
+
+  if type(n) == "number" or type(n) == "string" then
+    n = BignumBin.new(n)
+  end
+
+  local l = math.max(#self.data, #n.data)
+  local extra = 0
+  local num
+  for i = 1, l do
+    if i > #n.data then
+      num = self.data[i] + extra
+    elseif i > #self.data then
+      num = n.data[i] + extra
+    else
+      num = self.data[i] + n.data[i] + extra
+    end
+
+    local minus = num - 2 ^ (b.bits + 1)
+    if minus >= 0 then
+      b.data[i] = minus
+      extra = 1
+    else
+      b.data[i] = num
+      extra = 0
+    end
+  end
+
+  if extra > 0 then
+    b.data[l + 1] = extra
+  end
+
+  return b
+end
+--]]
+
+return BignumDec
--- problem_016.lua
+++ problem_016.lua
@@ -2,10 +2,7 @@ local helpers = require 'helpers'
 local Bignum = require 'bignum'
 
 local function answer ()
-  local b = Bignum.new(2)
-  for i = 2, 1000 do
-    b = b:add(b)
-  end
+  local b = Bignum.pow2(1000)
   local digits = b:tostring()
   local sum = 0
   for i = 1, #digits do
