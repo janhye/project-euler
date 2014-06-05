2983635b68274ec0178547483d6e5eeffdcba2e9
Ye Jian Hui <jan.h.ye@gmail.com>
2014-06-05 13:52:56 +0800

    updated 53

--- problem_053.lua
+++ problem_053.lua
@@ -1,28 +1,24 @@
 local helper = require "lib.helper"
 
----[=[
-require "lib._BigNum"
-local BigNum = BigNum
-local function factorial (n)
-  local r = BigNum.new(1)
-  for i = 1, n do
-    r = r * i
-  end
-
-  return r
-end
---]=]
-
+-- using pascal triangle solve
 local function answer ()
   local count = 0
-  local b = BigNum.new(1000000)
-  for n = 1, 100 do
-    for r = 1, n - 1 do
-      local c = factorial(n) / (factorial(r) * factorial(n - r))
-      if c > b then
-        count = count + 1
+  local pre = {1, 1} -- (n-1)-th line in pascal triangle
+  local nline = {} -- n-th line in pascal triangle
+  for n = 2, 100 do
+    for i = 1, n + 1 do
+      if i == 1 or i == n + 1 then
+        nline[i] = 1
+      else
+        local r = pre[i-1] + pre[i]
+        nline[i] = r
+        if r > 1000000 then
+          count = count + 1
+        end
       end
     end
+    pre = nline
+    nline = {}
   end
 
   return count
